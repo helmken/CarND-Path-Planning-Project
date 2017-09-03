52d0cc48fad867fa5c8721937d446d8a2285de30
dabbl <dabbl@home.de>
2017-09-03 16:34:47 +0200

    cleaned up waypoint_map

--- src/conversion_helpers.h
+++ src/conversion_helpers.h
@@ -1,11 +1,30 @@
 #ifndef CONVERSION_HELPERS_H
 #define CONVERSION_HELPERS_H
 
+#include <vector>
+
 // For converting back and forth between radians and degrees.
 constexpr double pi();
 
 double deg2rad(double x);
 
 double rad2deg(double x);
+
+double distance(double x1, double y1, double x2, double y2);
+
+/**
+* returns waypoint that has the smallest distance
+*/
+int ClosestWaypoint(
+    double x, double y,
+    std::vector<double> maps_x, std::vector<double> maps_y);
+
+/**
+* returns waypoint that makes most sense considering the current orientation
+*/
+int NextWaypoint(
+    double x, double y, double theta,
+    std::vector<double> maps_x, std::vector<double> maps_y);
+
 
 #endif // CONVERSION_HELPERS_H
--- src/waypoint_map.h
+++ src/waypoint_map.h
@@ -8,22 +8,6 @@
 #include <vector>
 
 
-double distance(double x1, double y1, double x2, double y2);
-
-/**
-* returns waypoint that has the smallest distance
-*/
-int ClosestWaypoint(
-    double x, double y,
-    std::vector<double> maps_x, std::vector<double> maps_y);
-
-/**
-* returns waypoint that makes most sense considering the current orientation
-*/
-int NextWaypoint(
-    double x, double y, double theta,
-    std::vector<double> maps_x, std::vector<double> maps_y);
-
 struct sMap
 {
     // Load up map values for waypoint's x,y,s and d normalized normal vectors
